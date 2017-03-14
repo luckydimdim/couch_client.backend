bcb8b96999d13bd76f8197f2471f73492f014fa0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-14 13:19:38 +0300

    refactoring

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -1,7 +1,7 @@
-:: msbuild /t:pack /p:Configuration=Release
-:: cd .\Cmas.Backend.Modules.Contracts.Datalayer.Couchdb\bin\Release\
-:: nuget push Cmas.Backend.Modules.Contracts.Datalayer.Couchdb.1.0.4.nupkg -Source http://cm-ylng-msk-04/nuget/nuget
+set project_name=couch_client.backend
+set nupkg_out_dir=bin\Debug
 
-cd .\Cmas.Backend.Modules.Contracts.Datalayer.Couchdb\bin\Debug\
-nuget push Cmas.Backend.Modules.Contracts.Datalayer.Couchdb.1.0.8.nupkg -Source http://cm-ylng-msk-04/nuget/nuget
+::dotnet pack %project_name%\%project_name%.csproj --output %nupkg_out_dir% --include-source --configuration Release
+dotnet nuget push %project_name%\%nupkg_out_dir%\*.nupkg --source http://cm-ylng-msk-04/nuget/nuget
+
 pause
